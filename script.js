313e9e072530a08c69e6c41a70afb5dfc865488d
vikash sahu <vikashsahusatna03@gmail.com>
2025-01-23 00:07:05 +0530

    it is the coommit from my local system

---
+++ script.js
@@ -0,0 +1,44 @@
+const questions=[ {
+    question: "who is appu";
+    answer:[
+        { taxt: "elephant" , correct:true },
+        { taxt: "rat" , correct:false },
+        { taxt: "cat" , correct:false },
+        { taxt: "dog" , correct:false }
+    ]
+},
+{
+    question: "what is apple";
+    answer:[
+        { taxt: "fruit" , correct:true },
+        { taxt: "rat" , correct:false },
+        { taxt: "cat" , correct:false },
+        { taxt: "dog" , correct:false }
+    ]
+},
+{
+    question: "what is chilli";
+    answer:[
+        { taxt: "vegitable" , correct:true },
+        { taxt: "rat" , correct:false },
+        { taxt: "cat" , correct:false },
+        { taxt: "dog" , correct:false }
+    ]
+},
+{
+    question: "who is narendra modi";
+    answer:[
+        { taxt: "prime minister" , correct:true },
+        { taxt: "rat" , correct:false },
+        { taxt: "cat" , correct:false },
+        { taxt: "dog" , correct:false }
+    ]
+}
+];
+
+const questionElement =document.getElementById("question");
+const answerButton =document.getElementById("answer-button");
+const nextButton =document.getElementById("next-btn");
+
+
+let currentQusetion
